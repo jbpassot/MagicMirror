de2fbec4972e183b25779dd3987b8cb42c9e593b
Jean-Baptiste Passot <jbpassot@gmail.com>
2021-05-02 14:42:32 -0700

    Squashed 'modules/MMM-Spotify/' content from commit c2947855
    git-subtree-dir: modules/MMM-Spotify
    git-subtree-split: c294785530c57a1a5ea803be6348499f35efb81e

---
+++ first_auth.js
@@ -0,0 +1,43 @@
+const fs = require("fs")
+const path = require("path")
+const Spotify = require("./Spotify.js")
+
+let file = path.resolve(__dirname, "spotify.config.json")
+let configurations = []
+
+if (fs.existsSync(file)) {
+    let configurators = JSON.parse(fs.readFileSync(file))
+    configurators.forEach(configurator => {
+        configurations.push(configurator)
+    })
+}
+else return console.log("[SPOTIFY] Error: please configure your spotify.config.json file")
+
+function authorize(configuration) {
+    return new Promise((resolve, reject) => {
+        new Spotify(configuration, true, true).authFlow().then(result => {
+            console.log(result)
+            resolve()
+        }, reason => {
+            console.log("[SPOTIFY - " + configuration.USERNAME + "] Error in authentication:")
+            console.log(reason)
+            reject()
+        })
+    })
+}
+
+async function authorizations(configurations) {
+    for (const configuration of configurations) {
+        try {
+            await authorize(configuration)
+        } catch (e) {
+          if (e) console.log('[SPOTIFY] ERROR: ', e)
+        }
+    }
+}
+
+authorizations(configurations).then(result => {
+    console.log('[SPOTIFY] Authorization process finished!')
+}, reason => {
+    console.log('[SPOTIFY] Authorization process failed!:', reason)
+})
